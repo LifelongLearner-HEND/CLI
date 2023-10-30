0d656d7e6c627df9b3e4c017568513c3b6dac15b
Hend Ahmed <92755637+LifelongLearner-HEND@users.noreply.github.com>
2023-10-30 19:00:51 +0200

    Add files via upload
    The rest of my commands are added.

--- Terminal.java
+++ Terminal.java
@@ -1,7 +1,5 @@
 import java.io.File;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -37,11 +35,17 @@ public class Terminal {
         }
     }
     void cdToHome() {
-        // Change the current directory to the user's home directory
-        String userHome = System.getProperty("user.home");
-        System.setProperty("user.dir", userHome);
-        currentDirectory = new File(userHome);
-        System.out.println("Current Directory: " + currentDirectory.getAbsolutePath());
+        // check if the user is in the home directory
+        if(currentDirectory.getAbsolutePath().equals(System.getProperty("user.home"))) {
+            System.out.println("You are already in the home directory!");
+        }
+        else {
+            // Change the current directory to the user's home directory
+            String userHome = System.getProperty("user.home");
+            System.setProperty("user.dir", userHome);
+            currentDirectory = new File(userHome);
+            System.out.println("Current Directory: " + currentDirectory.getAbsolutePath());
+        }
     }
     void cdToParent() {
         // Change the current directory to the parent directory
@@ -57,18 +61,93 @@ public class Terminal {
         System.out.println("Current Directory: " + currentDirectory.getAbsolutePath());
     }
     void cdToPath(String path) {
-        // Change the current directory to the given path
-        System.setProperty("user.dir", path);
-        currentDirectory = new File(System.getProperty("user.dir"));
-        System.out.println(currentDirectory.getAbsolutePath());
+        // check if the path exists
+        File file = new File(path);
+        if(!file.exists()) {
+            System.out.println("Path doesn't exits!");
+        }
+        else {
+            // Change the current directory to the given path
+            System.setProperty("user.dir", path);
+            currentDirectory = new File(System.getProperty("user.dir"));
+            System.out.println(currentDirectory.getAbsolutePath());
+        }
+    }
+    void touchAbs(String input) {
+        // create a file in the given absolute path
+        String []tempArg = parser.getArgs(input);
+        String filePath = tempArg[0];
+        // parse the path into the file name and the absolute path
+        String fileName = filePath.substring(filePath.lastIndexOf("\\") + 1);
+        System.out.println(fileName);
+        String path = filePath.substring(0, filePath.lastIndexOf("\\"));
+        System.out.println(path);
+        // check if the path exists
+        File file = new File(path);
+        if(!file.exists()) {
+            System.out.println("Path doesn't exits!");
+        }
+        else {
+            // add the file to the given path
+            file = new File(path, fileName);
+            try {
+                // check if the file is already created
+                boolean isCreated = file.createNewFile();
+                if(isCreated) {
+                    System.out.println("File created successfully!");
+                }
+                else {
+                    System.out.println("File already exists!");
+                }
+            } catch (IOException e) {
+                System.out.println("Error occurred!");
+            }
+        }
     }
     void touchRelative(String input) {
         String []tempArg = parser.getArgs(input);
-        String filePath = tempArg[0];
-        Path path = FileSystems.getDefault().getPath(filePath);
-        File file = path.toFile();
+        String relativePath = tempArg[0];
+        File file = new File(relativePath);
+        File parentDirectory = file.getParentFile();
+        // check if the directory exists
+        if(!parentDirectory.exists()) {
+            boolean created = parentDirectory.mkdirs();
+            if(created) {
+                System.out.println("Directory created successfully!");
+            }
+            else {
+                System.out.println("Error occurred!");
+            }
+        }
+        try {
+            // check if the file is already created
+            boolean isCreated = file.createNewFile();
+            if(isCreated) {
+                System.out.println("File created successfully!");
+            }
+            else {
+                System.out.println("File already exists!");
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred!");
+        }
     }
-    void touchAbs() {
-
+    void touchFileName(String input) {
+        String []tempArg = parser.getArgs(input);
+        String fileName = tempArg[0];
+        // create the file in the current directory
+        File file = new File(currentDirectory, fileName);
+        try {
+            // check if the file is already created
+            boolean isCreated = file.createNewFile();
+            if(isCreated) {
+                System.out.println("File created successfully!");
+            }
+            else {
+                System.out.println("File already exists!");
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred!");
+        }
     }
 }
