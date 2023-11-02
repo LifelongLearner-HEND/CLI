1a7afef7a9e37c8b1c32169d1e7f538a7e517a56
maro312 <102632948+maro312@users.noreply.github.com>
2023-11-02 19:59:57 +0200

    last

--- Terminal.java
+++ Terminal.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -308,6 +314,105 @@ public class Terminal {
         }
 
     }
+
+
+    Scanner scanner = new Scanner(System.in);
+    Parser parser = new Parser();
+    void echo(String[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    void cat(String[] arr){
+        if (arr.length == 1){
+            try {
+                File myObj = new File(arr[0]);
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+        else if (arr.length == 2){
+            try {
+                File file1 = new File(arr[0]);
+                Scanner myReader = new Scanner(file1);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                File file2 = new File(arr[1]);
+                myReader = new Scanner(file2);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+        else
+            System.out.println("An error occurred.");
+    }
+    void rm(String[] arr){
+        if (arr.length == 1){
+            File file = new File(arr[0]);
+            if (file.delete()) {
+                System.out.println("Deleted the file: " + file.getName());
+            } else {
+                System.out.println("An error occurred.");
+            }
+        }
+        else
+            System.out.println("An error occurred.");
+    }
+    void wc (String[] arr){
+        if (arr.length == 1){
+            int lines = 0,words = 0,characters = 0;
+            try {
+                File file = new File(arr[0]);
+                Scanner scl = new Scanner(file);
+                while (scl.hasNextLine()) {
+                    scl.nextLine();
+                    lines++;
+                }
+                Scanner scw = new Scanner(file);
+                while (scw.hasNext()){
+                    scw.next();
+                    words++;
+                }
+                Scanner scc = new Scanner(file);
+                while (scc.hasNext()){
+                    String temp = scc.nextLine();
+                    characters += temp.length();
+                }
+                scl.close();
+                scw.close();
+                scc.close();
+                System.out.print(lines);
+                System.out.print(" ");
+                System.out.print(words);
+                System.out.print(" ");
+                System.out.print(characters);
+                System.out.print(" ");
+                System.out.println(arr[0]);
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
     void chooseCommandAction() throws IOException {
         String input;
         Scanner scanner = new Scanner(System.in);
@@ -319,15 +424,15 @@ public class Terminal {
             if(parserObject.parse(parserObject.getCommandName())) {
                 commandHistory.add(input);
                 // echo command
-
-
-                // pwd command
-                if(parserObject.getCommandName().equals("pwd")){
+                if (parserObject.getCommandName().equals("echo"))
+                    echo(parserObject.getArgs(input));
+                    // pwd command
+                else if(parserObject.getCommandName().equals("pwd")){
                     System.out.println(pwd(parserObject.getArgs(input)));
                 }
 
                 // cd command
-                if(parserObject.getCommandName().equals("cd")) {
+                else if(parserObject.getCommandName().equals("cd")) {
                     cd(parserObject.getArgs(input));
                 }
                 // ls command
@@ -352,9 +457,13 @@ public class Terminal {
                 }
 
                 // cat command
-
+                else if (parserObject.getCommandName().equals("cat")){
+                    cat(parserObject.getArgs(input));
+                }
                 // wc command
-
+                else if (parserObject.getCommandName().equals("wc")) {
+                    wc(parserObject.getArgs(input));
+                }
                 // cp command
                 else if((parserObject.getCommandName().equals("cp"))){
                     cp(parserObject.getArgs(input));
@@ -366,7 +475,9 @@ public class Terminal {
                 }
 
                 // rm command
-
+                else if (parserObject.getCommandName().equals("rm")) {
+                    rm(parserObject.getArgs(input));
+                }
             }
             else {
                 System.out.println("Invalid Command!");
